5b33dcd57f6b32b3eaddf0861400da45f27e6c43
AkshK12 <rgpda123@gmail.com>
2026-01-12 15:45:59 +0530

    Add files via upload

---
+++ binary643.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == key)
+        {
+            return mid;   
+        }
+        else if (arr[mid] < key)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;    
+        }
+    }
+
+    return -1;   
+}
+
+int main()
+{
+    int arr[] = {5, 10, 15, 20, 25};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 20;
+
+    int index = binarySearch(arr, size, key);
+
+    if (index != -1)
+        cout << "Element found at index " << index;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
